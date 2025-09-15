0281ecda54f074bf851335f6e9543ea2858409ec
bruno o <bruno.o@fundamentalvr.com>
2025-09-15 16:46:22 +0100

    test: add Catch2 test scaffold and basic sanity test

---
+++ tests/sanity_tests.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_all.hpp>
+
+#include <vulkano/app.hpp>
+
+TEST_CASE("AppConfig default sizes are sane") {
+    const vulkano::AppConfig cfg { };
+    REQUIRE(cfg.width >= 640U);
+    REQUIRE(cfg.height >= 480U);
+}
+
